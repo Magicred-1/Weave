9221b363e4333bae34aef2f6b88793661637496b
Sakshi Shah <sakshi.shah299@gmail.com>
2024-01-16 16:33:29 +0530

    map issue resolved

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -11,7 +11,7 @@ const MapPage = () => {
     return (
         <>
             <Navbar />
-            {/* <DynamicMap /> */}
+            <DynamicMap />
             <Footer />
         </>
     )
