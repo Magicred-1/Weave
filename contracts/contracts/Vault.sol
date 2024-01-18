6284cb804b984deae38dc5a0fc3c5597b03d7239
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-01-19 00:18:52 +0100

    deposit ETH is working

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -7,41 +7,43 @@ import "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IP
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IVault.sol";
 
-contract Vault is Ownable, IVault {
-    address public GHO_TOKEN_ADDRESS = 0xc4bF5CbDaBE595361438F8c6a187bDc330539c60;
-    address public AAVE_POOL_ADDRESS = 0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf;
+interface IWETHGateway {
+  function depositETH(
+    address lendingPool,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external payable;
+}
 
-    address public WETH_TOKEN_ADDRESS = 0xc558dbdd856501fcd9aaf1e62eae57a9f0629a3c;
-    address public USDC_TOKEN_ADDRESS = 0x94a9d9ac8a22534e3faca9f4e7f2e2cf85d5e4c8;
-    address public EURS_TOKEN_ADDRESS = 0x6d906e526a4e2ca02097ba9d0caa3c382f52278e;
-    address public LINK_TOKEN_ADDRESS = 0xf8fb3713d459d7c1018bd0a49d19b4c44290ebe5;
+contract Vault is Ownable {
+    address public GHO_TOKEN_ADDRESS = 0xc4bF5CbDaBE595361438F8c6a187bDc330539c60;
+    address public WETH_TOKEN_ADDRESS = 0xC558DBdd856501FCd9aaF1E62eae57A9F0629a3c;
+    address public USDC_TOKEN_ADDRESS = 0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8;
+    address public EURS_TOKEN_ADDRESS = 0x6d906e526a4e2Ca02097BA9d0caA3c382F52278E;
+    address public LINK_TOKEN_ADDRESS = 0xf8Fb3713D459D7C1018BD0A49D19b4C44290EBE5;
+
+    address public leaderboardContractAddress;
 
     IPool internal constant aaveV3Pool = IPool(0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951);
+    IWETHGateway internal constant wETHGateway = IWETHGateway(0x387d311e47e80b498169e6fb51d3193167d89F7D);
 
     mapping(address => uint256) public pointsBalances;
 
-    boolean public withdrawAllowed = false;
+    modifier onlyLeaderboardContract() {
+        require(msg.sender == leaderboardContractAddress, "Caller is not leaderboard contract");
+        _;
+    }
 
-    address[] listOfAllowedTokens;
+    bool public withdrawAllowed = false;
 
     constructor(
-        address _USDC_TOKEN_ADDRESS,
-        address _EURS_TOKEN_ADDRESS,
-        address _LINK_TOKEN_ADDRESS,
-        address _WETH_TOKEN_ADDRESS
+        address _leaderboardContractAddress
     ) Ownable(msg.sender) {
-        listOfAllowedTokens.push(_USDC_TOKEN_ADDRESS);
-        listOfAllowedTokens.push(_EURS_TOKEN_ADDRESS);
-        listOfAllowedTokens.push(_LINK_TOKEN_ADDRESS);
-        listOfAllowedTokens.push(_WETH_TOKEN_ADDRESS);
+        leaderboardContractAddress = _leaderboardContractAddress;
     }
 
     function setGHOAddress(address _tokenAddress) external onlyOwner {
         GHO_TOKEN_ADDRESS = _tokenAddress;
-    }
-
-    function setAAVEPoolAddress(address _poolAddress) external onlyOwner {
-       AAVE_POOL_ADDRESS = _poolAddress;
     }
 
     // Risk Management
@@ -49,32 +51,28 @@ contract Vault is Ownable, IVault {
         withdrawAllowed = _withdrawAllowed;
     }
 
-    function addCollateral(address _token, uint256 _amount) external {
+    function addCollateral(address _tokenAddress, uint256 _amount) external {
        aaveV3Pool.deposit(_token, _amount, address(this), 0);
     }
 
     // Todo: Use WETHGateway to send non weth tokens to AAVE
     function addCollateralToAAVEPool(uint256 _amount, address _tokenAddress) external {
         require(_amount > 0, "Amount must be greater than 0");
-        require(isTokenAllowed(_tokenAddress), "Token not allowed");
-        require(IERC20(_tokenAddress).balanceOf(msg.sender) >= _amount, "Not enough balance");
-        require(IERC20(_tokenAddress).allowance(msg.sender, address(this)) >= _amount, "Not enough allowance");
-
-        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
-        IERC20(_tokenAddress).approve(AAVE_POOL_ADDRESS, _amount);
 
         aaveV3Pool.deposit(_tokenAddress, _amount, address(this), 0);
     }
 
-    function exchangePointsForGHO(uint256 _amount) external {
+    function exchangePointsForGHO(uint256 _amount) external onlyLeaderboardContract {
         require(pointsBalances[msg.sender] >= _amount, "Not enough points");
         pointsBalances[msg.sender] -= _amount;
         IERC20(GHO_TOKEN_ADDRESS).transfer(msg.sender, _amount);
     }
 
-    // function depositETH() external payable {
-    //     IWETH(WETH_TOKEN_ADDRESS).deposit{value: msg.value}();
-    // }
+    function depositETH() external payable {
+        IWETHGateway(wETHGateway).depositETH{
+            value: msg.value
+        }(address(aaveV3Pool), address(this), 0);
+    }
 
     function borrowAaveGHO(
             uint256 amount
@@ -84,6 +82,9 @@ contract Vault is Ownable, IVault {
     }
 
     function withdrawGHOFromContract(uint256 _amount) external onlyOwner {
+        require(withdrawAllowed, "Withdraw not allowed");
+        require(IERC20(GHO_TOKEN_ADDRESS).balanceOf(address(this)) >= _amount, "Not enough balance");
+
         IERC20(GHO_TOKEN_ADDRESS).transfer(msg.sender, _amount);
     }
 
@@ -95,10 +96,6 @@ contract Vault is Ownable, IVault {
         return IERC20(WETH_TOKEN_ADDRESS).balanceOf(address(this));
     }
 
-    function getAAVEPoolAddress() external view returns (address) {
-       return AAVE_POOL_ADDRESS;
-    }
-
     function getUserBalance(address _user) external view returns (uint256) {
         return pointsBalances[_user];
     }
@@ -107,12 +104,7 @@ contract Vault is Ownable, IVault {
         return GHO_TOKEN_ADDRESS;
     }
 
-    function isTokenAllowed(address _tokenAddress) internal view returns (bool) {
-        for (uint i = 0; i < listOfAllowedTokens.length; i++) {
-            if (listOfAllowedTokens[i] == _tokenAddress) {
-                return true;
-            }
-        }
-        return false;
+    function getWithdrawStatus() external view returns (bool) {
+        return withdrawAllowed;
     }
 }
