0eac21354e9890e02ab2dd294b17a7b1c4fe7034
Sakshi Shah <sakshi.shah299@gmail.com>
2024-01-19 11:51:49 +0530

    ui changed for footer and navabar

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,19 +8,19 @@ const Footer = () => {
 	return (
 		<footer className=" w-full h-full bg-gray-900  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-t border-gray-100">
 			<div className="w-full ">
-				<div className="sm:flex sm:items-center sm:justify-between px-20 py-4">
+				<div className="flex flex-col md:flex-row md:items-center md:justify-between justify-center md:px-20 py-4">
 					<span className={`${PressStart2P.className} self-center text-3xl font-bold whitespace-nowrap dark:text-white`}>
 						<a href='/'>Weave</a>
 					</span>
-					<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+					<span className="block text-sm text-gray-500 text-center dark:text-gray-400">
 						© 2023{' '}
 						<a href="/" className="hover:underline">
 							Weave
 						</a>
 					</span>
-					<ul className="flex flex-wrap items-center mb-6 text-lg font-medium text-gray-200 sm:mb-0 ">
+					<ul className="flex  justify-center mb-6 text-lg font-medium text-gray-200 sm:mb-0 ">
 						<li>
-							<a href="https://github.com/Magicred-1/Weave" target='_blank' className="hover:underline me-4 md:me-6 "><FaGithub /> Github
+							<a href="https://github.com/Magicred-1/Weave" target='_blank' className="hover:underline  flex gap-2"><FaGithub className='mt-2' /> Github
 							</a>
 						</li>
 					</ul>
