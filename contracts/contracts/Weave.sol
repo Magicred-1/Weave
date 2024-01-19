1f881ba295bd7db3569fc068b77faac299b5122d
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-01-19 10:03:13 +0100

    Onboarding contract done

--- contracts/contracts/Weave.sol
+++ contracts/contracts/Weave.sol
@@ -11,14 +11,6 @@ contract Users {
     }
 
     mapping(address => User) public users;
-    mapping(address => bool) public addressRegistered;
-
-    function registerUser(string memory nickname) external {
-        require(addressRegistered[msg.sender] == true , "Already Regsitered" );
-        users[msg.sender] = User(msg.sender , nickname, new address[](0),  new address[](0), 0);
-        addressRegistered[msg.sender] = true;
-    }
-
 
      // Function to set a user's nickname
     function setUsername(address userAddress, string memory newNickname) external {
@@ -35,46 +27,46 @@ contract Users {
         return users[userAddress].nickName;
     }  
 
+    // Need to be added to the event contract
+    // // Function to add an event to listOfEventsParticipated
+    // function addEventParticipated(address userAddress, address eventAddress) external  {
+    //     require(addressRegistered[userAddress], "User not registered");
+    //     users[userAddress].listOfEventsParticipated.push(eventAddress);
+    // }
 
-    // Function to add an event to listOfEventsParticipated
-    function addEventParticipated(address userAddress, address eventAddress) external  {
-        require(addressRegistered[userAddress], "User not registered");
-        users[userAddress].listOfEventsParticipated.push(eventAddress);
-    }
+    // // Function to add an event to listOfEventsAttended
+    // function addEventAttended(address userAddress, address eventAddress) external  {
+    //     require(addressRegistered[userAddress], "User not registered");
+    //     users[userAddress].listofEventsAttended.push(eventAddress);
+    // }
 
-    // Function to add an event to listOfEventsAttended
-    function addEventAttended(address userAddress, address eventAddress) external  {
-        require(addressRegistered[userAddress], "User not registered");
-        users[userAddress].listofEventsAttended.push(eventAddress);
-    }
-
-     // Function to update user points
-    function updatePoints(address userAddress, uint256 points) external  {
-        require(addressRegistered[userAddress], "User not registered");
-        users[userAddress].points += points;
-    }
+    //  // Function to update user points
+    // function updatePoints(address userAddress, uint256 points) external  {
+    //     require(addressRegistered[userAddress], "User not registered");
+    //     users[userAddress].points += points;
+    // }
 
 
-    // most Important
-    function claimRewards() public {
-        require(addressRegistered[msg.sender], "User not registered");
-        uint256 rewardPoints = users[msg.sender].points;
+    // // most Important
+    // function claimRewards() public {
+    //     require(addressRegistered[msg.sender], "User not registered");
+    //     uint256 rewardPoints = users[msg.sender].points;
 
-        // Add logic to handle the reward claim, like transferring tokens
+    //     // Add logic to handle the reward claim, like transferring tokens
 
-        // Reset points after claiming
-        users[msg.sender].points = 0;
-    }
+    //     // Reset points after claiming
+    //     users[msg.sender].points = 0;
+    // }
 
-    function participatedInEvent(address userAddress, address eventAddress) public view returns (bool) {
-        require(addressRegistered[userAddress], "User not registered");
-        for (uint i = 0; i < users[userAddress].listOfEventsParticipated.length; i++) {
-            if (users[userAddress].listOfEventsParticipated[i] == eventAddress) {
-                return true;
-            }
-        }
-        return false;
-    }   
+    // function participatedInEvent(address userAddress, address eventAddress) public view returns (bool) {
+    //     require(addressRegistered[userAddress], "User not registered");
+    //     for (uint i = 0; i < users[userAddress].listOfEventsParticipated.length; i++) {
+    //         if (users[userAddress].listOfEventsParticipated[i] == eventAddress) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }   
 
 
 
