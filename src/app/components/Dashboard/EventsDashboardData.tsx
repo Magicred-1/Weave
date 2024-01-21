770329a5238f65c5e089ce038c078189f93892f4
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-01-21 15:31:24 +0100

    fix

--- src/app/components/Dashboard/EventsDashboardData.tsx
+++ src/app/components/Dashboard/EventsDashboardData.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 type EventData = {
   eventName: string;
   eventDescription: string;
@@ -15,32 +14,3 @@ type ManagerData = {
   address: `0x${string}`;
   nickname?: string;
 };
-=======
-export interface EventsDashboardData {
-    eventName: string;
-    eventDescription: string;
-    eventStartDate: string;
-    eventEndDate: string;
-    eventLatitude: number; // Change to number
-    eventLongitude: number; // Change to number
-    eventRadius: number;
-    eventRadiusColor: string;
-    eventManagers: EventManagers[];
-  }
-  
-  // EventsDashboardComponent
-  const updatedEventsDatas: readonly EventData[] = (eventsInfos || []).map((eventInfo: any) => ({
-    eventName: eventInfo.eventName,
-    eventDescription: eventInfo.eventDescription,
-    eventStartDate: eventInfo.eventStartDate,
-    eventEndDate: eventInfo.eventEndDate,
-    eventLatitude: eventInfo.eventLatitude,
-    eventLongitude: eventInfo.eventLongitude,
-    eventRadius: eventInfo.eventRadius,
-    eventRadiusColor: eventInfo.eventRadiusColor,
-    eventManagers: (eventInfo.eventManagers || []).map((manager: any) => ({
-      address: manager.address,
-      nickname: manager.nickname || "", // Provide a default value for the nickname property
-    })),
-  }));
->>>>>>> 63a6049 (EventFactory Abi fix)
